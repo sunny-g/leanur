025b7c4c55b7a938063ccdec9dde287859d5c1e1
sunny-g <sunny.gonna@gmail.com>
2025-02-27 13:02:17 -0600

    minor reorg, starts jam/cue

---
+++ Leanur/Nock/Codec.lean
@@ -0,0 +1,13 @@
+import Leanur.Nock.Basic
+import Mathlib.Data.Nat.Bits
+
+namespace Leanur.Nock
+
+-- def Atom.jam (a : Atom) : List Bool :=
+--   let bits := a.bits -- bits, head is least-significant
+--   [] ++ [false]
+
+-- def Noun.jam : Noun → BitVec :=
+--   sorry
+
+end Leanur.Nock
